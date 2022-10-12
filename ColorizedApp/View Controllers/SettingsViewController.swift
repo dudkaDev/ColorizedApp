0fb1e5e48d4dfd21a8f5c0e5b4c981ea73bc4228
Andrey Abakumov <andrey9864rus@mail.ru>
2022-10-12 22:10:40 +0400

    Implemented logic for text fields without checking for letters and out of range. Add toolbar and "Done" button on keyboard.

--- ColorizedApp/View Controllers/SettingsViewController.swift
+++ ColorizedApp/View Controllers/SettingsViewController.swift
@@ -34,21 +34,30 @@ class SettingsViewController: UIViewController {
         colorResultView.backgroundColor = viewColor
         
         setSliders()
-        setValues()
+        setValueLabels()
+        
+        setValueTextFields()
         
         doneButton.layer.cornerRadius = 12
+        
+        redTextField.delegate = self
+        greenTextField.delegate = self
+        blueTextField.delegate = self
     }
     
     @IBAction func sliderAction(_ sender: UISlider) {
-        changeColor()
         switch sender {
         case redSlider:
             redValueLabel.text = string(from: sender)
+            redTextField.text = string(from: sender)
         case greenSlider:
             greenValueLabel.text = string(from: sender)
+            greenTextField.text = string(from: sender)
         default:
             blueValueLabel.text = string(from: sender)
+            blueTextField.text = string(from: sender)
         }
+        setColor()
     }
     
     @IBAction func doneButtonPresser() {
@@ -56,10 +65,10 @@ class SettingsViewController: UIViewController {
         dismiss(animated: true)
     }
 }
-    
-    //MARK: - Private Methods for sliders and labels
+
+//MARK: - Private Methods for sliders and labels
 extension SettingsViewController {
-    private func changeColor() {
+    private func setColor() {
         colorResultView.backgroundColor = UIColor(
             red: CGFloat(redSlider.value),
             green: CGFloat(greenSlider.value),
@@ -80,17 +89,83 @@ extension SettingsViewController {
         blueSlider.value = Float(ciColor.blue)
     }
     
-    private func setValues() {
+    private func setValueLabels() {
         redValueLabel.text = (String(format: "%.2f", redSlider.value))
         greenValueLabel.text = (String(format: "%.2f", greenSlider.value))
         blueValueLabel.text = (String(format: "%.2f", blueSlider.value))
     }
 }
 
-    //MARK: - Private Methods for text fields and keyboards
+//MARK: - Methods for text fields and keyboard
 extension SettingsViewController: UITextFieldDelegate {
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guard let text = textField.text else { return }
+        if text.isEmpty {
+            showAlert(
+                title: "Wrong format!",
+                message: "Please enter correct value",
+                textField: textField
+            )
+        }
+        
+        if let currentValue = Float(text) {
+            switch textField {
+            case redTextField:
+                redSlider.setValue(currentValue, animated: true)
+                redValueLabel.text = (String(format: "%.2f", redSlider.value))
+            case greenTextField:
+                greenSlider.setValue(currentValue, animated: true)
+                greenValueLabel.text = (String(format: "%.2f", greenSlider.value))
+            default:
+                blueSlider.setValue(currentValue, animated: true)
+                blueValueLabel.text = (String(format: "%.2f", blueSlider.value))
+            }
+            setColor()
+            return
+        }
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        textField.inputAccessoryView = toolBar
+        
+        let doneButton = UIBarButtonItem(
+            barButtonSystemItem: .done,
+            target: self,
+            action: #selector(didTapOne)
+        )
+        let flexibleSpace = UIBarButtonItem(
+            barButtonSystemItem: .flexibleSpace,
+            target: nil,
+            action: nil
+        )
+        
+        toolBar.items = [flexibleSpace, doneButton]
+    }
     
     @objc private func didTapOne() {
         view.endEditing(true)
+    }
+    
+    private func setValueTextFields() {
+        redTextField.text = (String(format: "%.2f", redSlider.value))
+        greenTextField.text = (String(format: "%.2f", greenSlider.value))
+        blueTextField.text = (String(format: "%.2f", blueSlider.value))
+    }
+    
+    private func showAlert(title: String, message: String, textField: UITextField? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            textField?.text = (String(format: "%.2f", self.redSlider.value))
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
     }
 }
